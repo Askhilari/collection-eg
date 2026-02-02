0b2ee2b7dae0c2c58de0a162b747c74bceb5a140
Askhilari <askhilari433@gmail.com>
2026-02-01 20:31:07 -0600

    Changed the mode functions code. Now it outputs what number occurs maximum. I used .iter() function and checked value for the maximum values. If they match then we have a mode!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 fn main() {
     let mut list = vec![2,10,9,3,1,1,2,3,1,1];
     println!("Original sequence is: {:?}", list);
-    let sorted_list = sorting_vec(&mut list);
+    let sorted_list = sort_var(&mut list);
     println!("Sorted Sequence is: {:?}", sorted_list);
     let sequence_median = get_median(&mut list);
     println!("Median of sequence is: {}", sequence_median);
     get_mode(&mut list);
 }
 
-fn sorting_vec(list: &mut Vec<i32>) -> &Vec<i32> {
-    for _ in 0..list.len(){
+// fn sorting_vec(list: &mut Vec<i32>) -> &Vec<i32> {
+//     for _ in 0..list.len(){
+//         for i in 0..(list.len() - 1){
+//             if list.get(i) > list.get(i+1) {
+//                 let temp = list[i+1];
+//                 list[i+1] = list[i];
+//                 list[i] = temp;
+//             }
+//         }
+//     }
+//     list
+// }
+
+
+fn sort_var(list: &mut Vec<i32>) -> &Vec<i32> {
+    for _ in 0..list.len() {
         for i in 0..(list.len() - 1){
-            if list.get(i) > list.get(i+1) {
-                let temp = list[i+1];
-                list[i+1] = list[i];
-                list[i] = temp;
+            if list[i] > list[i+1] {
+                let temp = list[i];
+                list[i] = list[i+1];
+                list[i+1] = temp;
             }
         }
     }
@@ -37,12 +51,17 @@ fn get_median(list: &mut Vec<i32>) -> i32 {
 }
 
 fn get_mode(list: &mut Vec<i32>) {
-    let mut map = HashMap::new();
+    let mut map: HashMap<&mut i32, i32> = HashMap::new();
     for i in list {
         let count = map.entry(i).or_insert(0);
         *count += 1;
     }
     let mode = map.values().copied().max();
+    for (key, val) in map.iter(){
+        if val == &mode.unwrap() {
+            println!("Number {key} occurs {val} times. Hence the mode is {key}")
+        }
+    }
     println!("{map:?}");
     match mode {
         Some(mode) => println!("{mode}"),
